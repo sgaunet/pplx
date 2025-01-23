2650e09af15941b56a2c1df262c093d10e872f8f
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2025-01-23 21:34:16 +0100

    Add markdown rendering functions for Perplexity API responses

--- pkg/console/console.go
+++ pkg/console/console.go
@@ -3,8 +3,15 @@ package console
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+
+	markdown "github.com/MichaelMure/go-term-markdown"
+	"github.com/sgaunet/perplexity-go/v2"
 )
+
+const DefaultLineLength = 80
+const DefaultLeftMargin = 6
 
 func Input(label string) (string, error) {
 	fmt.Printf("%s:\n", label)
@@ -22,4 +29,23 @@ func Input(label string) (string, error) {
 
 	err := scanner.Err()
 	return lines, err
+}
+
+func RenderAsMarkdown(pplxResponse *perplexity.CompletionResponse, output io.Writer) error {
+	result := markdown.Render(pplxResponse.GetLastContent(), DefaultLineLength, DefaultLeftMargin)
+	_, err := fmt.Fprintln(output, string(result))
+	if err != nil {
+		return fmt.Errorf("error writing markdown to output: %w", err)
+	}
+	return nil
+}
+
+func RenderCitations(pplxResponse *perplexity.CompletionResponse, output io.Writer) error {
+	for i, c := range pplxResponse.GetCitations() {
+		_, err := fmt.Fprintf(output, "[%d]: %s\n", i, c)
+		if err != nil {
+			return fmt.Errorf("error writing citations as markdown to output: %w", err)
+		}
+	}
+	return nil
 }
